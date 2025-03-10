120afa425bc98e31cf7fa7c5fd6025d27d89c64f
Raj Jaiswal <rajaiswal62@gmail.com>
2025-03-10 17:20:52 +0530

    Fixed Hamburg

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ function Dashboard() {
   return (
     <div className="flex h-screen bg-white overflow-hidden">
       {/* Mobile sidebar toggle */}
-      <div className="lg:hidden fixed top-4 left-4 z-50">
+      <div className="lg:hidden fixed top-4 left-4">
         <button onClick={() => setIsSidebarOpen(!isSidebarOpen)} className="p-2 rounded-md bg-white shadow-md">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M3 18H21V16H3V18ZM3 13H21V11H3V13ZM3 6V8H21V6H3Z" fill="#1A1A1A" />
